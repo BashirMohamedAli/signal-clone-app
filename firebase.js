7da7f2acce6d7d59dbe2ac476054f5eabae2efad
Bashir Mohamed Ali <62313798+BashirMohamedAli@users.noreply.github.com>
2022-09-02 11:54:43 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpbzwTaVrFXCXI6HoEWOCV66E5DhYQsWQ",
-  authDomain: "chat-app-build.firebaseapp.com",
-  projectId: "chat-app-build",
-  storageBucket: "chat-app-build.appspot.com",
-  messagingSenderId: "919057975692",
-  appId: "1:919057975692:web:59ade824266a96b3c7eea9",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 let app;
